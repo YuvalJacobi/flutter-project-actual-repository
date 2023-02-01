62c884fa9cc978cd1d21b7f74df1d345b6d95c90
Yuval Jacobi <yuvaljacobi777@gmail.com>
2023-02-01 09:20:25 +0200

    temp commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter_complete_guide/screens/auth_screen.dart';
 
 void main() => runApp(MyApp());
