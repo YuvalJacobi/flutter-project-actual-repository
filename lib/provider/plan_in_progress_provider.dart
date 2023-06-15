7cc9264a0dca9a8dbd48130dab24affe8991342d
Yuval-Jacobi <yuvaljacobi777@gmail.com>
2023-06-15 15:16:17 +0300

    g

--- lib/provider/plan_in_progress_provider.dart
+++ lib/provider/plan_in_progress_provider.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/model/exercise_in_plan.dart';
 import 'package:provider/provider.dart';
